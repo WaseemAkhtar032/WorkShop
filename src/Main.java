a9cc42e177da78e90c0606f4850b5326a7c05b75
WaseemAkhtar032 <113421575+WaseemAkhtar032@users.noreply.github.com>
2022-10-18 20:46:03 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("Welcome to Workshop 1");
+        InputString s = new InputString();
+        int[] q = s.inputString();
+
+        SmallestLargest sl = new SmallestLargest();
+        sl.smallestLargest(q);
     }
 }
